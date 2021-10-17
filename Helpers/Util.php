6ac4d32772fdce9ff40cb05559c4b3c0de6c5e0c
echowebid <saya@echo.web.id>
2021-10-17 15:53:52 +0700

    add helpers util

---
+++ Helpers/Util.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @author Eko Triono <saya@echo.web.id>
+ */
+
+namespace Helpers;
+
+class Util
+{
+    /**
+     * Validate a given email address
+     * @param string $email
+     * @return bool
+     * related global function is_email($email)
+     */
+    public static function isEmail($email)
+    {
+        return (filter_var($email, FILTER_VALIDATE_EMAIL) !== false) ? true : false;
+    }
+
+    /**
+     * Dump & Die
+     * @param mixed $var
+     */
+    public static function dd($var)
+    {
+        if (is_bool($var)) {
+            $var = 'bool(' . ($var ? 'true' : 'false') . ')';
+        }
+
+        if (php_sapi_name() === 'cli') {
+            print_r($var);
+        } else {
+            highlight_string("<?php\n" . var_export($var, true));
+        }
+        die();
+    }
+}
